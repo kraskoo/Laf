cc552871d3f6e9ed6cde14d5ac66cb253ca558ea
kraskoo <kraskoo87@gmail.com>
2019-10-23 00:45:22 +0300

    API: Remove last migration (unnecessary)

--- API/LafAPI.Web/LafAPI.Web/Models/Message/MessageViewModel.cs
+++ API/LafAPI.Web/LafAPI.Web/Models/Message/MessageViewModel.cs
@@ -2,13 +2,9 @@
 {
     using System;
 
-    using AutoMapper;
-
-    using LafAPI.Common.Mapping;
-    using LafAPI.Data.Models;
     using LafAPI.Web.Models.Account;
 
-    public class MessageViewModel : IMapFrom<Message>, IHaveCustomMappings
+    public class MessageViewModel
     {
         public int Id =>
             $"{this.User.Id}-{this.Friend.Id}({this.CreationDate.Ticks}): {this.Text}".GetHashCode();
@@ -20,11 +16,6 @@
         public DateTime CreationDate { get; set; }
 
         public string Text { get; set; }
-
-        public void CreateMappings(IProfileExpression configuration) =>
-            configuration.CreateMap<Message, MessageViewModel>()
-                .ForMember(m => m.User, opt => opt.MapFrom(x => x.User))
-                .ForMember(m => m.Friend, opt => opt.MapFrom(x => x.Friend));
 
         public override int GetHashCode() => this.Id;
     }
